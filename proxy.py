a5ca1494064123e490d2d3ac2ee931aedf8adb58
0han <0han@protonmail.com>
2018-05-17 15:20:31 +1000

    rewrite the proxy module and you can use it to generate an ip proxy pool and use it for your project. Btw, it contains a test module to see if it useable, you can delete it if you want

--- proxy.py
+++ proxy.py
@@ -1,24 +1,47 @@
-#Get_Proxy_list by 0han
-#date="2017.4.29"
-#coding:utf-8 python 3.5 
+#Proxy Module for collecting proxy ip and put them into a pool
+#__data__=='16/5/2018'
+#utf-8
 import requests
 import json
-url="http://gimmeproxy.com/api/getProxy"
-s=requests.session()
-def get_proxy(country):
-	print("[*] Get Annoymous IP Proxy")
-	mainurl="https://gimmeproxy.com/api/getProxy?get=true&anonymityLevel=0&country="+country
-	r=s.get(mainurl,verify=True)
-	dic=json.loads(r.text)
-	dic_main={dic["protocol"]:dic["curl"]}
-	print(dic_main)
-	return dic_main
-#list > table > tbody > tr:nth-child(1) > td.style1
-
+from random import randint
+"""
+    The function proxy_pool is going to take an argument which indicates the amount 
+    of the ip proxy addresses you want to have in the pool and return a list that contains
+    ip proxyies without overloop.
+"""
+def proxy_pool(amount_of_ip):
+    pool=[]
+    print("[*] Get",str(amount_of_ip),"Annoymous IP Proxy")
+    mainurl="https://gimmeproxy.com/api/getProxy?get=true&anonymityLevel=1&supportsHttps=True"
+    for i in range(amount_of_ip):
+        s=requests.session()
+        r=json.loads(s.get(mainurl,verify=True).text)
+        res='https://'+r['ipPort']
+        s.close()
+        if res not in pool:
+            pool.append(res)
+        else:
+            print("[!] IP proxy address ",r['ipPort'],"is already in the pool")
+            continue
+    return pool # pool here is a list contains n's (the amount you indicate) ip in the form like "https://80.211.184.190:8888"
+"""
+    The test function is used to get a random ip from the pool and test whether it useable
+    测试模块，在项目中可参看内置函数return_random_ipproxy的用法，从pool代理池中随机提取一个作为代理ip
+"""
 def test():
-	ip=get_proxy("US")
-	url='https://api.ipify.org?format=json'
-	res=s.get(url,proxies=ip,verify=True)
-	dic=json.loads(res.text)
-	print(dic["ip"])
+    def return_random_ipproxy(pool,num):
+        res={}
+        ipPool = pool
+        res['https']=ipPool[randint(0,num)]
+        return res
+    ipPool=return_random_ipproxy(proxy_pool(5),5)
+    print(ipPool)
+    urlforlocalip='https://api.ipify.org?format=json'
+    s=requests.session()
+    try:
+        res=s.get(urlforlocalip,proxies=ipPool,verify=True)
+        dic=json.loads(res.text)
+        print("[+] Your local ip address is",dic["ip"])
+    except ProxyError:
+        print("Can not connect to the ip proxy")
 test()
